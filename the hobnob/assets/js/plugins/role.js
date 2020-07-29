704e3a3caade47ef3c73ebcc0141fa04ffd84824
vaibhavagg2311 <vaibhavagg2311@gmail.com>
2020-07-29 20:29:42 +0530

    hello

---
+++ the hobnob/assets/js/plugins/role.js
@@ -0,0 +1,20 @@
+$("#seeAnotherField").change(function() {
+  if ($(this).val() == "yes") {
+    $('#otherFieldDiv').show();
+    $('#otherField').attr('required', '');
+    $('#otherField').attr('data-error', 'This field is required.');
+  } else {
+    $('#otherFieldDiv').hide();
+    $('#otherField').removeAttr('required');
+    $('#otherField').removeAttr('data-error');
+  }
+});
+$("#seeAnotherField").trigger("change");
+
+$("#seeAnotherFieldGroup").change(function() {
+  if ($(this).val() == "yes") {
+    $('#otherFieldGroupDiv').show();
+    $('#otherField1').attr('required', '');
+    $('#otherField1').attr('data-error', 'This field is required.');
+    $('#otherField2').attr('required', '');
+    $('#otherField2').attr('data-error', 'This field is required.');
---
+++ the hobnob/assets/js/plugins/username.js
@@ -0,0 +1,9 @@
+$(function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false
+    });
+
+    $( "#opener" ).click(function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  });
