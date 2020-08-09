c3d0bc1e0c27e23e419a870831808b35a60b6e8a
Janus Jerom <janussmartboy@gmail.com>
2020-08-09 21:37:30 +0530

    updated js with fixes

--- the hobnob/examples/Marketing/JS/script.js
+++ the hobnob/examples/Marketing/JS/script.js
@@ -1,80 +1,61 @@
-var currentTab = 0;
-showTab(currentTab);
+var currentTab = 0; 
+showTab(currentTab); 
 
-function showTab (n) {
-    var x = document.getElementsByClassName("tab"); // name of the main form
-    x[n].style.display = "block";
-
-    if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
-    } else {
-        document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n==(x.length - 1)){
-        document.getElementById("nextBtn").innerHTML = "Submit";
-    } else {
-        document.getElementById(nextBtn).innerHTML = "Next";
-    }
-    fixStepIndicator(n)  //to display correct step
-}
-
-function nextPrev(n){
-    var x = document.getElementsByClassName("tab"); // name of the main form
-    if (n==1 && !validateForm()) return false;
-    x[currentTab].style.display = "none";
-    currentTab = currentTab + n;
-    if (currentTab >= x.length){
-        document.getElementById("form").submit(); //form Name
-        return false;
-    }
-
-    showTab(currentTab);
-}
-
-function validateForm(){
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab"); // name of the main form
-    y = document.getElementsByClassName("input") //input of the form
-
-    for(i = 0; i<y.length; i++){
-        if (y[i].value == ""){
-            y[i].className += " invalid";
-            valid = false;
-        }
-    }
-
-    if(valid) {
-        document.getElementsByClassName("step")[currentTab].className += " finish"; // An indicator span to identify the step completed and know current status
-    }
-    return valid;
-}
-
-function fixStepIndicator (n) {
-    var i, x = document.getElementsByClassName("steps");
-    for (i=0; i < x.length; i++) {
-        x[i].className = x[i].className.replace(" active", "")
-    }
-    x[n].className += " active";
-}
-
-
-
-//OVERLAY IF NEEDED
-
-function on() {
-    document.getElementById("overlay").style.display = "block";
+// It will make sure that previous button appears after 1st tab
+// continue button at the last page changes to submit
+function showTab(n) {
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
   }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  } 
-
-
-// select onclick
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Continue";
   }
-}, false);
 
+  fixStepIndicator(n)
+}
+
+// Function to know which step to display
+function nextPrev(n) {
+  var x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) return false;
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  if (currentTab >= x.length) {
+    document.getElementById("regForm").submit();
+    return false;
+  }
+
+  showTab(currentTab);
+}
+
+ // Function deals with validation of the form 
+function validateForm() {
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  for (i = 0; i < y.length; i++) {
+    if (y[i].value == "") {
+      y[i].className += " invalid";
+      valid = false;
+    }
+  }
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; 
+}
+
+// Function removes the "active" class of all steps
+function fixStepIndicator(n) {
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  x[n].className += " active";
+}
